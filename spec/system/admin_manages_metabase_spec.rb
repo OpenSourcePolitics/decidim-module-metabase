08607dabb76579651df8469c3257dfe0f539aa97
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-12-08 17:10:46 +0100

    Add not found message in dashboard index

--- spec/system/admin_manages_metabase_spec.rb
+++ spec/system/admin_manages_metabase_spec.rb
@@ -5,6 +5,23 @@ require "spec_helper"
 describe "Admin manages Metabase", type: :system do
   let(:organization) { create(:organization) }
 
+  let(:enabled) { true }
+  let(:metabase_site_url) { "http://fake.site.url" }
+  let(:metabase_secret_key) { "fake_secret_key" }
+  let(:metabase_dashboard_ids) { [1] }
+  let(:metabase_secrets) do
+    {
+      enabled: enabled,
+      site_url: metabase_site_url,
+      secret_key: metabase_secret_key,
+      dashboard_ids: metabase_dashboard_ids
+    }
+  end
+
+  before do
+    allow(Rails.application.secrets).to receive(:metabase).and_return(metabase_secrets)
+  end
+
   describe "with an adminstrator" do
     let!(:admin) { create(:user, :admin, :confirmed, organization: organization) }
 
@@ -23,12 +40,21 @@ describe "Admin manages Metabase", type: :system do
 
     context "when accessing metabase dashboard" do
       before do
+        stub_request(:any, /fake.site.url/).to_return(body: "Not found", status: 404)
         find("a[href='/admin/metabase/']").click
       end
 
       it "renders the index view" do
         within ".card#metabase" do
-          expect(page).to have_content("Metabase#Index")
+          expect(page).to have_selector("iframe")
+        end
+      end
+
+      context "when there is no dashboards" do
+        let(:metabase_dashboard_ids) { [] }
+
+        it "display not found message" do
+          expect(page).to have_content("No dashboard found")
         end
       end
     end
